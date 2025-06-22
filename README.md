b5bba1ef449f8045fad5f42532604d3a3bea0903
snopflake <nofanisrina@gmail.com>
2025-06-22 18:35:32 +0700

    first init

---
+++ README.md
@@ -0,0 +1,3 @@
+# animation_playground
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/animation_playground/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.animation_playground
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
